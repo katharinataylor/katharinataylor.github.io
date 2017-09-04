3ffd53c914ad9f66c640a0f189a7f3601fde17df
Katharina Taylor <katharinataylor@Katharinas-MacBook-Pro.local>
2017-09-04 20:57:20 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+// Project gallery
+
+var showMore1 = document.querySelector('.button-1');
+var photoOverlay1 = document.querySelector('.overlay-1');
+var projDescription1 = document.getElementById('gallery-item-1');
+var arrow1 = document.querySelector('.arrow-1');
+
+showMore1.addEventListener('click', function() {
+	showMore1.style.display = 'none';
+	photoOverlay1.style.opacity = 0;
+	projDescription1.style.display = 'flex';
+});
+arrow1.addEventListener('click', function() {
+	showMore1.style.display = 'block';
+	photoOverlay1.style.opacity = 0.8;
+	projDescription1.style.display = 'none';
+});
+
+var showMore2 = document.querySelector('.button-2');
+var photoOverlay2 = document.querySelector('.overlay-2');
+var projDescription2 = document.getElementById('gallery-item-2');
+var arrow2 = document.querySelector('.arrow-2');
+
+showMore2.addEventListener('click', function() {
+	showMore2.style.display = 'none';
+	photoOverlay2.style.opacity = 0;
+	projDescription2.style.display = 'flex';
+});
+arrow2.addEventListener('click', function() {
+	showMore2.style.display = 'block';
+	photoOverlay2.style.opacity = 0.8;
+	projDescription2.style.display = 'none';
+});
+
+var showMore3 = document.querySelector('.button-3');
+var photoOverlay3 = document.querySelector('.overlay-3');
+var projDescription3 = document.getElementById('gallery-item-3');
+var arrow3 = document.querySelector('.arrow-3');
+
+showMore3.addEventListener('click', function() {
+	showMore3.style.display = 'none';
+	photoOverlay3.style.opacity = 0;
+	projDescription3.style.display = 'flex';
+});
+arrow3.addEventListener('click', function() {
+	showMore3.style.display = 'block';
+	photoOverlay3.style.opacity = 0.8;
+	projDescription3.style.display = 'none';
+});
+
+var showMore4 = document.querySelector('.button-4');
+var photoOverlay4 = document.querySelector('.overlay-4');
+var projDescription4 = document.getElementById('gallery-item-4');
+var arrow4 = document.querySelector('.arrow-4');
+
+showMore4.addEventListener('click', function() {
+	showMore4.style.display = 'none';
+	photoOverlay4.style.opacity = 0;
+	projDescription4.style.display = 'flex';
+});
+arrow4.addEventListener('click', function() {
+	showMore4.style.display = 'block';
+	photoOverlay4.style.opacity = 0.8;
+	projDescription4.style.display = 'none';
+});
+
+var showMore5 = document.querySelector('.button-5');
+var photoOverlay5 = document.querySelector('.overlay-5');
+var projDescription5 = document.getElementById('gallery-item-5');
+var arrow5 = document.querySelector('.arrow-5');
+
+showMore5.addEventListener('click', function() {
+	showMore5.style.display = 'none';
+	photoOverlay5.style.opacity = 0;
+	projDescription5.style.display = 'flex';
+});
+arrow5.addEventListener('click', function() {
+	showMore5.style.display = 'block';
+	photoOverlay5.style.opacity = 0.8;
+	projDescription5.style.display = 'none';
+});
+
+var showMore6 = document.querySelector('.button-6');
+var photoOverlay6 = document.querySelector('.overlay-6');
+var projDescription6 = document.getElementById('gallery-item-6');
+var arrow6 = document.querySelector('.arrow-6');
+
+showMore6.addEventListener('click', function() {
+	showMore6.style.display = 'none';
+	photoOverlay6.style.opacity = 0;
+	projDescription6.style.display = 'flex';
+});
+arrow6.addEventListener('click', function() {
+	showMore6.style.display = 'block';
+	photoOverlay6.style.opacity = 0.8;
+	projDescription6.style.display = 'none';
+});
