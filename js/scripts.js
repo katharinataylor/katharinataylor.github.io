ff53239ba49c341a25c4aae596a45497b82bb061
Katharina Taylor <katharinataylor@Katharinas-MacBook-Pro.local>
2017-12-27 22:54:07 +0100

    new version of website

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,97 @@
 // Project gallery
 
-var showMore1 = document.querySelector('.button-1');
-var photoOverlay1 = document.querySelector('.overlay-1');
-var projDescription1 = document.getElementById('gallery-item-1');
 var arrow1 = document.querySelector('.arrow-1');
+var projDescription1 = document.getElementById('gallery-item-1');
 
-showMore1.addEventListener('click', function() {
-	showMore1.style.display = 'none';
-	photoOverlay1.style.opacity = 0;
-	projDescription1.style.display = 'flex';
-});
 arrow1.addEventListener('click', function() {
-	showMore1.style.display = 'block';
-	photoOverlay1.style.opacity = 0.8;
-	projDescription1.style.display = 'none';
+
+	if (arrow1.className !== 'clicked') {
+		projDescription1.style.display = 'flex';
+		arrow1.style.transform = 'rotate(-135deg)';
+		arrow1.className = 'clicked';
+	} else if (arrow1.className === 'clicked') {
+		projDescription1.style.display = 'none';
+		arrow1.style.transform = 'rotate(-315deg)';
+		arrow1.className = 'unclicked';
+	}
 });
 
-var showMore2 = document.querySelector('.button-2');
-var photoOverlay2 = document.querySelector('.overlay-2');
-var projDescription2 = document.getElementById('gallery-item-2');
 var arrow2 = document.querySelector('.arrow-2');
+var projDescription2 = document.getElementById('gallery-item-2');
 
-showMore2.addEventListener('click', function() {
-	showMore2.style.display = 'none';
-	photoOverlay2.style.opacity = 0;
-	projDescription2.style.display = 'flex';
-});
 arrow2.addEventListener('click', function() {
-	showMore2.style.display = 'block';
-	photoOverlay2.style.opacity = 0.8;
-	projDescription2.style.display = 'none';
+	
+	if (arrow2.className !== 'clicked') {
+		projDescription2.style.display = 'flex';
+		arrow2.style.transform = 'rotate(-135deg)';
+		arrow2.className = 'clicked';
+	} else if (arrow2.className === 'clicked') {
+		projDescription2.style.display = 'none';
+		arrow2.style.transform = 'rotate(-315deg)';
+		arrow2.className = 'unclicked';
+	}
 });
 
-var showMore3 = document.querySelector('.button-3');
-var photoOverlay3 = document.querySelector('.overlay-3');
-var projDescription3 = document.getElementById('gallery-item-3');
 var arrow3 = document.querySelector('.arrow-3');
+var projDescription3 = document.getElementById('gallery-item-3');
 
-showMore3.addEventListener('click', function() {
-	showMore3.style.display = 'none';
-	photoOverlay3.style.opacity = 0;
-	projDescription3.style.display = 'flex';
-});
 arrow3.addEventListener('click', function() {
-	showMore3.style.display = 'block';
-	photoOverlay3.style.opacity = 0.8;
-	projDescription3.style.display = 'none';
+	
+	if (arrow3.className !== 'clicked') {
+		projDescription3.style.display = 'flex';
+		arrow3.style.transform = 'rotate(-135deg)';
+		arrow3.className = 'clicked';
+	} else if (arrow3.className === 'clicked') {
+		projDescription3.style.display = 'none';
+		arrow3.style.transform = 'rotate(-315deg)';
+		arrow3.className = 'unclicked';
+	}
 });
 
-var showMore4 = document.querySelector('.button-4');
-var photoOverlay4 = document.querySelector('.overlay-4');
-var projDescription4 = document.getElementById('gallery-item-4');
 var arrow4 = document.querySelector('.arrow-4');
+var projDescription4 = document.getElementById('gallery-item-4');
 
-showMore4.addEventListener('click', function() {
-	showMore4.style.display = 'none';
-	photoOverlay4.style.opacity = 0;
-	projDescription4.style.display = 'flex';
-});
 arrow4.addEventListener('click', function() {
-	showMore4.style.display = 'block';
-	photoOverlay4.style.opacity = 0.8;
-	projDescription4.style.display = 'none';
+	
+	if (arrow4.className !== 'clicked') {
+		projDescription4.style.display = 'flex';
+		arrow4.style.transform = 'rotate(-135deg)';
+		arrow4.className = 'clicked';
+	} else if (arrow4.className === 'clicked') {
+		projDescription4.style.display = 'none';
+		arrow4.style.transform = 'rotate(-315deg)';
+		arrow4.className = 'unclicked';
+	}
 });
 
-var showMore5 = document.querySelector('.button-5');
-var photoOverlay5 = document.querySelector('.overlay-5');
-var projDescription5 = document.getElementById('gallery-item-5');
 var arrow5 = document.querySelector('.arrow-5');
+var projDescription5 = document.getElementById('gallery-item-5');
 
-showMore5.addEventListener('click', function() {
-	showMore5.style.display = 'none';
-	photoOverlay5.style.opacity = 0;
-	projDescription5.style.display = 'flex';
-});
 arrow5.addEventListener('click', function() {
-	showMore5.style.display = 'block';
-	photoOverlay5.style.opacity = 0.8;
-	projDescription5.style.display = 'none';
+	
+	if (arrow5.className !== 'clicked') {
+		projDescription5.style.display = 'flex';
+		arrow5.style.transform = 'rotate(-135deg)';
+		arrow5.className = 'clicked';
+	} else if (arrow5.className === 'clicked') {
+		projDescription5.style.display = 'none';
+		arrow5.style.transform = 'rotate(-315deg)';
+		arrow5.className = 'unclicked';
+	}
 });
 
-var showMore6 = document.querySelector('.button-6');
-var photoOverlay6 = document.querySelector('.overlay-6');
-var projDescription6 = document.getElementById('gallery-item-6');
 var arrow6 = document.querySelector('.arrow-6');
+var projDescription6 = document.getElementById('gallery-item-6');
 
-showMore6.addEventListener('click', function() {
-	showMore6.style.display = 'none';
-	photoOverlay6.style.opacity = 0;
-	projDescription6.style.display = 'flex';
-});
 arrow6.addEventListener('click', function() {
-	showMore6.style.display = 'block';
-	photoOverlay6.style.opacity = 0.8;
-	projDescription6.style.display = 'none';
+	
+	if (arrow6.className !== 'clicked') {
+		projDescription6.style.display = 'flex';
+		arrow6.style.transform = 'rotate(-135deg)';
+		arrow6.className = 'clicked';
+	} else if (arrow6.className === 'clicked') {
+		projDescription6.style.display = 'none';
+		arrow6.style.transform = 'rotate(-315deg)';
+		arrow6.className = 'unclicked';
+	}
 });
